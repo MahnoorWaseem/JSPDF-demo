6df7912dd1394ad455148fcbfd8382de268a2d6b
Mahnoor Waseem <mahnoorwaseem60@gmail.com>
2024-12-25 14:17:42 +0500

    pdf presentation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import MyTable from './MyTable';
+import SimpleDataGrid from './NewTable';
+import SimpleDataGrid2 from './NewTable copy';
+
 
 function App() {
   return (
     <div>
       <h1>Material-UI Table to PDF</h1>
-      <MyTable />
+      {/* <MyTable /> */}
+      <SimpleDataGrid />
+      {/* <SimpleDataGrid2 /> */}
+
     </div>
   );
 }
